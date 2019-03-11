3afe21d74a70c6b3e4d4900dd278216f33211395
Jose Javier Lirio <jliriovi@everis.com>
2019-03-11 20:12:23 +0000

    Structural equality (== and .equals() ) and Referential equality (=== and !==)

--- VariablesDeclaration/src/example/kotlin/variablesdeclaration/Declarations.kt
+++ VariablesDeclaration/src/example/kotlin/variablesdeclaration/Declarations.kt
@@ -28,13 +28,38 @@ fun main(args: Array<String>) {
     employees = mutableSetOf()
     employees.add(employee1)
     employees.forEach { println("id: " + it.id + ", " + "name: " + it.name) }
+    println()
 
     // immutable ArrayList
     val names = arrayListOf("Jose", "Luis", "Sarah")
     print(names[1]) // Indexing the ArrayList without using .get() just like we usually do with Arrays in Java
+    println()
 
+    val employeeOne = Employee(555, "Laura")
+    val employeeTwo = Employee(666, "Manuel")
+    val employeeThree = Employee(666, "Manuel")
+    println()
+    // == is using equals so is doing a structural equality
+    println(employeeOne == employeeTwo)
+    println(employeeTwo == employeeThree)
+    println(employeeOne.equals(employeeTwo))
+    println(employeeTwo.equals(employeeThree))
+    // === is doing a referential equality
+    println(employeeOne === employeeTwo)
+    println(employeeTwo === employeeThree)
+    val employeeFour = employeeTwo
+    println(employeeFour != employeeTwo)
+    println(employeeFour !== employeeTwo)
+    println(employeeTwo === employeeThree)
+    println(employeeTwo === employeeThree)
 }
 
 class Employee(val id: Int, var name: String) {
 
-}
+    override fun equals(obj: Any?): Boolean {
+        if(obj is Employee) {
+            return name == obj.name && id == obj.id
+        }
+        return false
+    }
+ }
