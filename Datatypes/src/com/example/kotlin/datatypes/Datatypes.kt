c0ef810dcd9c925cd895a931198205252515201f
Jose Javier Lirio <jliriovi@everis.com>
2019-03-18 16:07:53 +0000

    - Arrays

--- Datatypes/src/com/example/kotlin/datatypes/Datatypes.kt
+++ Datatypes/src/com/example/kotlin/datatypes/Datatypes.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin.datatypes
 
 import com.example.kotlin.javacode.DummyClass
+import java.math.BigDecimal
 
 
 fun main(args: Array<String>) {
@@ -65,4 +66,70 @@ fun main(args: Array<String>) {
     val onVacation = DummyClass().isVacationTime(vacationTime)
     println()
     println(onVacation)
+
+
+    /*** Array class ***/
+
+    val names = arrayOf("John", "Jane", "Jill", "Joe")
+
+    val longs1 = arrayOf(1L, 2L, 3L)
+    val longs2 = arrayOf<Long>(1, 2, 3, 4)
+    val longs3 = arrayOf(1, 2, 3, 4)
+    println()
+    println(longs1 is Array<Long>)
+    println(longs2 is Array<Long>)
+    // println(longs3 is Array<Long>) // The compiler tells us that not is an Array<Long>
+    println(longs3 is Array<Int>)
+
+    // Usually arrays indexing
+    println(longs1[2])
+
+    // Initializing an array using a lambda expression
+    // This lambda expression is multiplying the index of 16 elements by 2
+    val evenNumbers = Array(16){ i -> i * 2 }
+    println("Printing evenNumbers array: ")
+    for (number in evenNumbers) {
+        println(number)
+    }
+    println(evenNumbers is Array<Int>)
+
+    val lotsOfNumbers = Array(100000) { i -> i + 1 } // 1, 2, 3, ...100000
+    // val allZeroes = Array(100) { i -> 0 } // 0, 0, 0, 0...
+    val allZeroes = Array(100) { 0 } // 0, 0, 0, 0...
+
+    var someArray: Array<Int>
+    someArray = arrayOf(1, 2, 3, 4)
+    println("Printing someArray array: ")
+    for (number in someArray) {
+        println(number)
+    }
+
+    someArray = Array(6) { i -> (i + 1) * 10 }
+    println("Printing someArray array: ")
+    for (number in someArray) {
+        println(number)
+    }
+
+
+    // Mixing data types in an Array: array of Any
+    val mixedArray = arrayOf("hello", 22, BigDecimal(10.5), 'a')
+    println("Printing mixedArray array: ")
+    for (elem in mixedArray) {
+        println(elem)
+    }
+    println(mixedArray is Array<Any>)
+
+    val myIntArray = intArrayOf(3, 9, 434, 2, 33) // array that map a primitive array: int[]
+    DummyClass().printNumbers(myIntArray)
+
+    var someOtherArray = IntArray(5) // [0, 0, 0, 0, 0]
+    println("Printing someOtherArray array: ")
+    for (number in someOtherArray) {
+        println(number)
+    }
+
+    // We are converting an array of general Array class to an array that map a primitive array: int[]
+    DummyClass().printNumbers(evenNumbers.toIntArray())
+
+    val convertedIntArray: Array<Int> = myIntArray.toTypedArray()
 }
--- Datatypes/src/com/example/kotlin/javacode/DummyClass.java
+++ Datatypes/src/com/example/kotlin/javacode/DummyClass.java
@@ -5,4 +5,11 @@ public class DummyClass {
     public String isVacationTime(boolean onVacation) {
         return onVacation ? "I am on vacation" : "I am working";
     }
+
+    public void printNumbers(int[] numbers) {
+        System.out.println("Printing int[] numbers array:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
 }
