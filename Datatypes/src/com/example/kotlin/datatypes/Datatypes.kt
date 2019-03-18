eb984c156ceaf902586cad0870276bd14d65155e
Jose Javier Lirio <jliriovi@everis.com>
2019-03-18 14:14:47 +0000

    Char data type.

--- Datatypes/src/com/example/kotlin/datatypes/Datatypes.kt
+++ Datatypes/src/com/example/kotlin/datatypes/Datatypes.kt
@@ -3,6 +3,8 @@ package com.example.kotlin.datatypes
 
 fun main(args: Array<String>) {
 
+    /*** Numeric data types ***/
+
     val myInt = 10
     print("default datatype is ${myInt is Int}")
     var myLong = 22L
@@ -36,4 +38,21 @@ fun main(args: Array<String>) {
     // Reminder: There is no automatic widening. The values are not automatically widened when you are assigning them to a wider data type.
     // You have to explicitly do the widening by calling the appropriate two data type function
     myDouble = myFloat.toDouble()
+
+
+    /*** Char data types ***/
+
+    val char = 'b'
+
+    //In Java is representing the UTF-8 and ASCII character A.
+    // char c = 65;
+
+    // In Kotlin
+    val myCharInt = 65
+    // If you have a character literal as a numeric and you want to actually get it into a char variable you have to explicitly do that
+    // there is no automatic conversion done for us
+    print(myCharInt.toChar())
+
+
+
 }
