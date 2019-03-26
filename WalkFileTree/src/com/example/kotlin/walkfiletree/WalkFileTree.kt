59f21f8d08f70e1eb19b9959773f7bc350375ba1
USERSAD\jliriovi <jliriovi@everis.com>
2019-03-26 18:00:44 +0000

    Walking top-down the directories and files

--- WalkFileTree/src/com/example/kotlin/walkfiletree/WalkFileTree.kt
+++ WalkFileTree/src/com/example/kotlin/walkfiletree/WalkFileTree.kt
@@ -1,7 +1,19 @@
 package com.example.kotlin.walkfiletree
 
+import java.io.File
 
 
 fun main(args: Array<String>) {
 
+    // Starting at working directory
+    // Because we are walking top-down the directories are visited before the files
+    File(".").walkTopDown().forEach { println(it) }
+
+
+    // Printing filtered files
+    println()
+    println("Printing only Kotlin files...")
+    File(".").walkTopDown()
+                       .filter { it.name.endsWith(".kt") }
+                       .forEach { println(it) }
 }
