729bea72eb68e5cd17b3b9f6b3db9c0f16cd52d2
USERSAD\jliriovi <jliriovi@everis.com>
2019-03-26 17:22:12 +0000

    - Reading binary files.

--- BinaryIO/src/com/example/kotlin/binaryio/BinaryIO.kt
+++ BinaryIO/src/com/example/kotlin/binaryio/BinaryIO.kt
@@ -1,7 +1,24 @@
 package com.example.kotlin.binaryio
 
+import java.io.DataInputStream
+import java.io.EOFException
+import java.io.FileInputStream
+
 
 fun main(args: Array<String>) {
 
 
+    val di = DataInputStream(FileInputStream("testfile.bin"))
+    var si: String
+
+    try {
+        while (true) {
+            si = di.readUTF()
+            println(si)
+        }
+    }
+    catch (e: EOFException) {
+
+    }
+    
 }
