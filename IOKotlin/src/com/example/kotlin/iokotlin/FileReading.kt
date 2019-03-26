b312909e95a6e1a57b0428fd17159f2ce478419b
USERSAD\jliriovi <jliriovi@everis.com>
2019-03-26 16:36:07 +0000

    Adding 'testfile.txt'

--- IOKotlin/src/com/example/kotlin/iokotlin/FileReading.kt
+++ IOKotlin/src/com/example/kotlin/iokotlin/FileReading.kt
@@ -4,4 +4,6 @@ package com.example.kotlin.iokotlin
 
 fun main(args: Array<String>) {
 
+    // Kotlin does not have its own IO classes so what is done is it has basically extension functions onto the existing
+    // Java IO classes so when we do IO in Kotlin we are always using the IO classes in the JDK. Package 'kotlin.io'
 }
