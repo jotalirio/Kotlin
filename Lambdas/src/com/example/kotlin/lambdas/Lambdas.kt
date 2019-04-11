bacc359a4bec6bea4c41b4848ff56fda272b48c3
jliriovi <jliriovi@everis.com>
2019-04-11 15:14:39 +0100

    New project 'Lambdas' created

--- Lambdas/src/com/example/kotlin/lambdas/Lambdas.kt
+++ Lambdas/src/com/example/kotlin/lambdas/Lambdas.kt
@@ -1,2 +1,9 @@
 package com.example.kotlin.lambdas
 
+
+fun main(args: Array<String>) {
+
+    // Lambdas in Kotlin have to be within curly braces and we can store them in variables and we can also call them
+    // directly using the 'run' function
+    
+}
