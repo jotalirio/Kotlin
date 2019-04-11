e502db760ef22b48cd5650b930c90caf6fe86086
jliriovi <jliriovi@everis.com>
2019-04-11 15:09:23 +0100

    New project 'CollectionsLambdasGenerics' created

---
+++ CollectionsLambdasGenerics/src/com/example/kotlin/collectionslambdasgenerics/CollectionsLambdasGenerics.kt
@@ -0,0 +1,8 @@
+package com.example.kotlin.collectionslambdasgenerics
+
+// List, Map, Set and everything else that's all based on the Java classes but Kotlin has added functions that make
+// working with the collection classes a lot more convenient
+
+fun main(args: Array<String>) {
+
+}
